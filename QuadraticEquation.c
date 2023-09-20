1a68eaecbfb2fbb31c308787ac779100088ec9fd
Navneet Vishwakarma <rajvl132011@gmail.com>
2023-09-20 14:49:32 +0530

    To find Roots of Quadratic Equation

---
+++ QuadraticEquation.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+
+    int a,b,c,x1,x2,d,n;
+   
+
+    printf("Enter Variable a : ");
+    scanf("%d",&a);
+    printf("Enter Variable b : ");
+    scanf("%d",&b);
+    printf("Enter Variable c : ");
+    scanf("%d",&c);
+
+    d=(b*b)-(4*a*c);
+   
+    n = sqrt(d);
+
+    x1 = (-b+n)/(2*a);
+    x2 = (-b-n)/(2*a);
+
+    
+  
+    
+    
+    printf("x1 = %d\nx2 = %d",x1,x2);    
+
+    return 0;
+}
