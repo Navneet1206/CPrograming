6d06737daaec2b8182cc9cd7f1c65130c036a4fe
Navneet Vishwakarma <rajvl132011@gmail.com>
2023-09-21 13:17:28 +0530

    Modification and making perfects

--- lFdigit.c
+++ lFdigit.c
@@ -2,26 +2,23 @@
 
 int main()
 {
-    int i,n,c=0,l,f;
+    int i,n,l,f,pro=0;
 
     printf("Enter any number : ");
     scanf("%d",&n);
+    
      l=n%10;
  
-    for (i=0;i<=n;i++)
+    for (i=n;i!=0;i/=10)
     {
-     c++;
-     n=n/10;
+     
+        f=i%10;
+
     }
      
-    f=n;
     
-    if(n<=10)
-    {
+    
 
-        f=n%10;
-  
-    }
     printf("\nfirst digit  = %d  \nlast digit = %d",f,l);
     
    return 0;
